f51f82259580a74ed0d36652df9e9713dadfbc19
Angel <2162556631@qq.com>
2023-07-12 16:18:00 +0800

    UI UX canvas design

--- Assets/Scripts/ObjectFollow.cs
+++ Assets/Scripts/ObjectFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObjectFollow : MonoBehaviour
 {
-    Vector3 offsetPos = new Vector3(-5.4f,45.3f,-38.5f);
+    Vector3 offsetPos = new Vector3(-7.6f,56f,-49.8f);
     public Transform target;
     void Update()
     {
